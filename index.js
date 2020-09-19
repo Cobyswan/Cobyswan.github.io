7b7b3680809fc9683dce3b5d077f35a5f0dc0e10
unknown <cobyswan@yahoo.com>
2020-09-19 10:20:50 -0700

    removed index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let video = document.querySelector("video");
-const gif = document.createElement('div');
-gif.innerHTML = '<img class="gifBackground" src="https://media.giphy.com/media/GRpBKrZne3AfCbuF6R/giphy.gif" alt="deeplake"></img>';
-if (screen.width < 460) {
-    console.log(document.querySelector('video'))
-    console.log('smaller')
-    video.parentNode.replaceChild(gif, video)
-}
